f6551cf3153555c5f2cbae637fad8a51d5867db2
barbsjean <barbarajbrake@gmail.com>
2024-04-20 14:00:24 -0500

    working through question 3

---
+++ scooter_extra_tab_for_scribbles.sql
@@ -0,0 +1,428 @@
+
+WITH bird_May_1 AS
+(
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+), 
+
+WITH bird_May_1 AS
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-02'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-03'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-04'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-05'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-06'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-07'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-08'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-09'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-10'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-11'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-12'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-13'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-14'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-15'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-16'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-17'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-18'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-19'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-20'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-21'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-22'
+group by companyname
+
+WITH bird_May_1 AS
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-23'
+group by companyname
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-24'
+group by companyname
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-25'
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-26'
+group by companyname
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-27'
+group by companyname
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-28'
+group by companyname
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-29'
+group by companyname
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-30'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-31'
+group by companyname
+
+
+
+
+
+
+
+
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- scooter_hawaiian_SQL.sql
+++ scooter_hawaiian_SQL.sql
@@ -71,6 +71,10 @@ or create_dt IS NULL
 
 /* - What date range is represented in each of the date columns? Investigate any values that seem odd. */
 
+select count(distinct startdate)
+from trips 
+
+
 select 
 	min(pubdatetime) as first_date,
 	max(pubdatetime) as last_date
@@ -214,13 +218,21 @@ from scooters
 -- where pubdatetime <= '05/31/2019' 
 group by companyname, date_time
 
+select 
+	companyname,
+	sumdid as scooter_id,
+	to_char(pubdatetime, 'MM/DD/YY')::date as date_time
+from scooters
+limit 10
+
+
 
 
 WITH May_scooters AS
 
 (	SELECT
 		companyname, 
-		COUNT(distinct scooter_id) as number_of_scooters_for_May
+		COUNT(scooter_id) as number_of_scooters_for_May
 	FROM (select companyname, sumdid as scooter_id, to_char(pubdatetime, 'MM/DD/YY')::date as date_time from scooters where sumdgroup ilike 'scooter')
  	WHERE date_time <= '05/31/2019'
  	GROUP BY companyname
@@ -230,7 +242,7 @@ June_scooters AS
 
 (	SELECT
 		companyname, 
-		COUNT(distinct scooter_id) as number_of_scooters_for_June
+		COUNT(scooter_id) as number_of_scooters_for_June
 	FROM (select companyname, sumdid as scooter_id, to_char(pubdatetime, 'MM/DD/YY')::date as date_time from scooters where sumdgroup ilike 'scooter')
  	WHERE date_time BETWEEN '06/01/2019' AND '06/30/2019'
  	GROUP BY companyname
@@ -240,31 +252,98 @@ July_scooters AS
 
 (	SELECT
 		companyname, 
-		COUNT(distinct scooter_id) as number_of_scooters_for_July
+		COUNT(scooter_id) as number_of_scooters_for_July
 	FROM (select companyname, sumdid as scooter_id, to_char(pubdatetime, 'MM/DD/YY')::date as date_time from scooters where sumdgroup ilike 'scooter')
  	WHERE date_time >= '07/01/2019'
 	GROUP BY companyname
 )
 
-SELECT *
+SELECT *, SUM(number_of_scooters_for_May + number_of_scooters_for_June + number_of_scooters_for_July) as total_usage
 FROM May_scooters
 FULL JOIN June_scooters
 USING (companyname)
 FULL JOIN July_scooters
 USING (companyname)
+GROUP BY companyname, number_of_scooters_for_May, number_of_scooters_for_June, number_of_scooters_for_July
+
+***********************************
+Companyname   May   June   July
+
+"Bird"		 3064	2910   2583
+"Bolt"	     346	333	    276
+"Gotcha"	 223	223	    224
+"Jump"	     976	1036	1206
+"Lime"	    1631	1455	902
+"Lyft"	    1376	1152	1026
+"Spin"	    687	    713	    570
+***********************************
 
 
+--------------------------------------------------------------------
+
+/* 2. According to Second Substitute Bill BL2018-1202, all permitted operators will first clean data before providing or reporting data to Metro. Data processing and cleaning shall include:  
+
+* Removal of staff servicing and test trips  
+* Removal of trips below one minute  
+* Trip lengths are capped at 24 hours  
+Are the scooter companies in compliance with the second and third part of this rule? */
+
+select companyname, count(pubtimestamp)
+from trips
+where tripduration < 1.0 or tripduration > 1440
+group by companyname
+/* 9154 */
+
+
+--------------------------------------------------------------------
+
+/* 3. The goal of Metro Nashville is to have each scooter used a minimum of 3 times per day. Based on the data, what is the average number of trips per scooter per day? Make sure to consider the days that a scooter was available. How does this vary by company? */      /* USE TRIPS TABLE :) */
+
+select count(pubtimestamp)
+from trips
+/* 565522 */   /* breaks down each trip but has the long/lat within it's own column vs broken down */
+	
 
 select 
 	companyname,
-	sumdid as scooter_id,
-	to_char(pubdatetime, 'MM/DD/YY')::date as date_time
-from scooters
+	count(distinct triprecordnum) as number_of_trip_records,
+	count(distinct sumdid) as number_of_scooter_ids,
+	count(distinct triprecordnum) / count(distinct sumdid) as AVG_per_day
+from trips
+where tripduration > 1.0 or tripduration < 1440
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) number_of_trip_records
+from trips
+group by companyname
+
+
+select companyname, count(distinct triprecordnum) as number_of_trip_records
+from trips
+where companyname = 'Bird'
+	and startdate = '2019-05-01'
+group by companyname
+
+
+select startdate, 
+	DATE_TRUNC('day', startdate) as day
+from trips
 limit 10
 
 
-	
-		
+
+
+--------------------------------------------------------------------
+
+/* 4. Metro would like to know how many scooters are needed, and something that could help with this is knowing peak demand. Estimate the highest count of scooters being used at the same time. When were the highest volume times? Does this vary by zip code or other geographic region? */
+
+select *
+from trips
+limit 10
+
+
+select 
 
 
 
